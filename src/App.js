49355a4afdaa893efa21d7e3ec3a5bdd860c2252
Jakub Godlewski <jakub.godlewski@yahoo.com>
2022-07-08 18:15:45 +0200

    add routes to pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import {Route, Routes, BrowserRouter} from "react-router-dom";
+import Home from "./pages/home/Home";
+import Course from "./pages/course/Course";
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/course/:id" element={<Course/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
