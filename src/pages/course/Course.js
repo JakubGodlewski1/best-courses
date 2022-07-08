662422efcaa740d4b120460a28672709a043fe35
Jakub Godlewski <jakub.godlewski@yahoo.com>
2022-07-08 20:23:52 +0200

    create specific course page

--- src/pages/course/Course.js
+++ src/pages/course/Course.js
@@ -1,11 +1,20 @@
 import "./Course.css"
 import {useFetch} from "../../hooks/useFetch";
+import {useParams} from "react-router-dom";
 
 const Course = () => {
-     const {data} = useFetch("")
+    const {id} = useParams()
+
+     const {data: course} = useFetch(`http://localhost:3000/courses/${id}`)
 
     return (
-        <div>
+        <div className="course">
+            {course && <>
+                <h2 style={{margin: "50px 0"}} className="title">{course.name}</h2>
+                <p>Price: {course.price}</p>
+                <p>Duration: {course.duration}</p>
+                <p>{course.description}</p>
+            </>}
 
         </div>
     );
