2054ae06f8fedc1d5e8c433a981d48211e260fb0
Jakub Godlewski <jakub.godlewski@yahoo.com>
2022-07-08 20:06:33 +0200

    create CourseList component and add it to home page

---
+++ src/components/courseList/CourseList.js
@@ -0,0 +1,26 @@
+import "./CourseList.css"
+import {useFetch} from "../../hooks/useFetch";
+import {Link} from "react-router-dom";
+
+const CourseList = () => {
+    const {data, isPending, error} = useFetch("http://localhost:3000/courses")
+
+
+    return (
+        <div className="course-list">
+            {isPending && <p>Loading...</p>}
+            {error && <p>{error}</p>}
+            {data && data.map((course)=> (
+                <Link to={`/course/${course.id}`} className="card" key={course.id}>
+                   <h2 className="title">{course.name}</h2>
+                    <h3>Price: {course.price}</h3>
+                    <h3>Duration: {course.duration}</h3>
+                    <p>{course.description.slice(0,50)}</p>
+                </Link>
+                )
+            )}
+        </div>
+    );
+};
+
+export default CourseList;
